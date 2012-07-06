f3e91c9efd629b70a93e060a04dd7bc8c5d51408
Andre Nathan <andre@digirati.com.br>
2012-07-06 08:52:21 -0300

    Rename slave process

--- src/spfd.ml
+++ src/spfd.ml
@@ -2,7 +2,7 @@ open Lwt
 open Printf
 
 (* TODO configuration file *)
-let spf_exec = sprintf "%s/_build/src/spf-slave" (Unix.getcwd ())
+let spf_exec = sprintf "%s/_build/src/spf-policyd" (Unix.getcwd ())
 let lock_file = "/tmp/spfd.pid"
 let num_slaves = 4
 let listen_socket = "/tmp/spf.socket" 
